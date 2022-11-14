f28611879d3a74ef674b06a4fedf6854c8807604
jfbenitezz <98917513+jfbenitezz@users.noreply.github.com>
2022-11-13 21:45:19 -0500

    Add files via upload

---
+++ heap.py
@@ -0,0 +1,43 @@
+import random
+def BuildHeap(arr, n, i):
+	large = i 
+	left = 2 * i + 1 
+	right = 2 * i + 2 
+
+	if (left < n) and (arr[i] < arr[left]):
+		large = left
+
+	if right < n and arr[large] < arr[right]:
+		large = right
+
+	if large != i:
+		(arr[i], arr[large]) = (arr[large], arr[i]) 
+		BuildHeap(arr, n, large)
+
+def HeapSort(arr):
+	n = len(arr)
+
+	for i in range(n // 2 - 1, -1, -1):
+		BuildHeap(arr, n, i)
+
+	for i in range(n - 1, 0, -1):
+		(arr[i], arr[0]) = (arr[0], arr[i])
+		BuildHeap(arr, i, 0)
+
+def Show(arr):
+	for i in range(len(arr)):
+		print(arr[i], end=" ")
+	print()   
+
+
+if __name__ == '__main__':
+    arr = []
+    for i in range(100):
+        arr.append(random.randint(-100000,100000))
+    print("Lista dada:", end="\n")
+    Show(arr)
+    HeapSort(arr)
+    print("Lista ordenada ascendentemente: ", end="\n")
+    Show(arr)
+    
+
---
+++ sort.py
@@ -0,0 +1,43 @@
+import random
+def MergeSort(arr):
+	if len(arr) > 1:
+		mid = len(arr)//2
+		left = arr[:mid]
+		right = arr[mid:]
+		MergeSort(left)
+		MergeSort(right)
+		i = j = k = 0
+
+		while i < len(left) and j < len(right):
+			if left[i] <= right[j]:
+				arr[k] = left[i]
+				i += 1
+			else:
+				arr[k] = right[j]
+				j += 1
+			k += 1
+
+		while i < len(left):
+			arr[k] = left[i]
+			i += 1
+			k += 1
+
+		while j < len(right):
+			arr[k] = right[j]
+			j += 1
+			k += 1
+
+def Show(arr):
+	for i in range(len(arr)):
+		print(arr[i], end=" ")
+	print()
+
+if __name__ == '__main__':
+    arr = []
+    for i in range(100):
+        arr.append(random.randint(-100000,100000))
+    print("Lista dada:", end="\n")
+    Show(arr)
+    MergeSort(arr)
+    print("Lista ordenada ascendentemente: ", end="\n")
+    Show(arr)
