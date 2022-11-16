aaf548ba87a07e52677b686c72be5d549bf37049
jfbenitezz <98917513+jfbenitezz@users.noreply.github.com>
2022-11-15 23:02:23 -0500

    Add files via upload

--- quick.py
+++ quick.py
@@ -1,4 +1,5 @@
 import random 
+import copy
 def rightPartition(array, low, high):
 	pivot = array[high]
 	i = low - 1
@@ -18,21 +19,89 @@ def leftPartition(array, low, high):
     (array[pivot], array[low]) = (array[low], array[pivot])
     return pivot
 
-def quickSort(array, low, high):
-	if low < high:
-        #Para seleccionar pivote inicial escoger entre left y right metodos
-		pi = leftPartition(array, low, high)
-		quickSort(array, low, pi - 1)
-		quickSort(array, pi + 1, high)
+def midPartition(arr, low, high):
+    pivot = arr[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while arr[i] < pivot:
+            i += 1
+
+        j -= 1
+        while arr[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+        arr[i], arr[j] = arr[j], arr[i]
+
+def dualPartition(arr, low, high):
+	
+	if arr[low] > arr[high]:
+		arr[low], arr[high] = arr[high], arr[low]
+		
+	j = k = low + 1
+	g, p, q = high - 1, arr[low], arr[high]
+	
+	while k <= g:
+		if arr[k] < p:
+			arr[k], arr[j] = arr[j], arr[k]
+			j += 1
+
+		elif arr[k] >= q:
+			while arr[g] > q and k < g:
+				g -= 1	
+			arr[k], arr[g] = arr[g], arr[k]
+			g -= 1
+
+			if arr[k] < p:
+				arr[k], arr[j] = arr[j], arr[k]
+				j += 1				
+		k += 1	
+	j -= 1
+	g += 1
+	arr[low], arr[j] = arr[j], arr[low]
+	arr[high], arr[g] = arr[g], arr[high]
+	return j, g
+
+def quickSort(array, low, high,op):
+    if low < high:
+        if(op=="1"):
+            pi = leftPartition(array, low, high)
+            quickSort(array, low, pi - 1, op)
+            quickSort(array, pi + 1, high, op)           
+        if(op=="2"):
+            pi = rightPartition(array, low, high)
+            quickSort(array, low, pi - 1, op)
+            quickSort(array, pi + 1, high, op)
+        if(op=="3"):
+            pi = midPartition(array, low, high) 
+            quickSort(array, low, pi, op)
+            quickSort(array, pi + 1, high, op)
+        if(op=="4"):
+            lp, rp = dualPartition(arr, low, high)
+            quickSort(arr, low, lp - 1,op)
+            quickSort(arr, lp + 1, rp - 1,op)
+            quickSort(arr, rp + 1, high,op)
+
+
+
+
 
 
 if __name__ == '__main__':
     arr = []
-    for i in range(20):
+    for i in range(50):
         arr.append(random.randint(-100000,100000))
-    print("Lista dada:", end="\n")
-    print(arr)
-    quickSort(arr, 0, len(arr) - 1)
-    print("Lista ordenada ascendentemente: ", end="\n")
-    print(arr)
-    
+    print(f'{arr} original')
+    test = copy.deepcopy(arr)
+    test.sort()
+    op = input("Select a the pivot")
+    quickSort(arr, 0, len(arr) - 1, op)
+    if test == arr:
+        print("bueno")
+    else:
+        print("malo")
+    print(f'{arr} ordenado')
+
